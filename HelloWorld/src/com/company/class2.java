8e235c7b9fdc2195ad5511ae1dfb5af3ac32f0a5
anhkt181095 <72240874+anhkt181095@users.noreply.github.com>
2020-11-05 10:12:40 +0700

    new

---
+++ HelloWorld/src/com/company/class2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class class2 {
+    public static void main(String[] args) {
+        double x = 16 –2;
+        if (x < 16) {
+            System.out.println("A");
+        } else if (x < 15) {
+            System.out.println("B");
+        } else {
+            System.out.println("C");
+        }
+    }
---
+++ untitled/src/SystemtemTime.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class SystemtemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+
+    }
+}
+
+
